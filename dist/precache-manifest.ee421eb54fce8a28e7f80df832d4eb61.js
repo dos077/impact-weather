d99f184f5e308e7ce089ea953af00d2252edf641
Zh <zeming@dreamofsky.com>
2020-07-30 23:35:58 -0400

    commit dist for gh-pages

---
+++ dist/precache-manifest.ee421eb54fce8a28e7f80df832d4eb61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "265e6ba3298f21783a19",
+    "url": "/css/app.fc8553d0.css"
+  },
+  {
+    "revision": "9a7ab71194ea7170bd5e",
+    "url": "/css/chunk-vendors.830f4569.css"
+  },
+  {
+    "revision": "143653dcb5e7c6c01549ce52e94abaae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "265e6ba3298f21783a19",
+    "url": "/js/app.8afd6811.js"
+  },
+  {
+    "revision": "9a7ab71194ea7170bd5e",
+    "url": "/js/chunk-vendors.dda7f575.js"
+  },
+  {
+    "revision": "724bd22bbc98cb0d0ea7f03ddb32a36e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
