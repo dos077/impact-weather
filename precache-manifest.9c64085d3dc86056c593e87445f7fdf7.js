e7fcca72441f8be7c677d41db306570412eb7c68
Zh <zeming@dreamofsky.com>
2020-08-01 00:11:03 -0400

    commit dist for gh-pages

---
+++ precache-manifest.9c64085d3dc86056c593e87445f7fdf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3a0f48374bd20d616c",
+    "url": "/impact-weather/css/app.b81c08eb.css"
+  },
+  {
+    "revision": "8eddbe475305c5f4fc51",
+    "url": "/impact-weather/css/chunk-vendors.830f4569.css"
+  },
+  {
+    "revision": "e58a7083d0b5c709eec2b84158d06827",
+    "url": "/impact-weather/index.html"
+  },
+  {
+    "revision": "6d3a0f48374bd20d616c",
+    "url": "/impact-weather/js/app.a0c139bc.js"
+  },
+  {
+    "revision": "8eddbe475305c5f4fc51",
+    "url": "/impact-weather/js/chunk-vendors.e44a65b4.js"
+  },
+  {
+    "revision": "02dd20dda30214ea016a8d594ba9ca86",
+    "url": "/impact-weather/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/impact-weather/robots.txt"
+  }
+]);
